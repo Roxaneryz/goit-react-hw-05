9a30b104bbab517778ee618ccff5c26f382160dd
Roxaneryz <roxane5666@yahoo.gr>
2024-06-23 11:55:17 +0300

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:moviesId" element={<MovieDetailsPage />}>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
             <Route path="cast" element={<MovieCast />} />
             <Route path="reviews" element={<MovieReviews />} />
           </Route>
